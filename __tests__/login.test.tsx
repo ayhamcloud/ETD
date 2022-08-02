c8f5dad046f47ba0050d0c388d5a556fc2d47207
Simon Röhrl <simon.roehrl01@gmail.com>
2022-08-02 16:18:27 +0200

    fix: update jest snapshots

--- __tests__/login.test.tsx
+++ __tests__/login.test.tsx
@@ -3,11 +3,12 @@ import SignIn from '@/components/SignIn'
 
 describe('Login', () => {
   it('renders Login',async () => {
-    render(<SignIn forgotPassword="Forgot password?" title="ETD - Sign In" metaDescription="Sign in to ETD" noAccountYet=" Don&apos;t have an account? Sign Up" next={null} router={null}/>)
+    render(<SignIn forgotPassword="Forgot password?" title="ETD - Sign In" metaDescription="Sign in to ETD" noAccountYet="Don't have an account? Sign Up" next={null} router={null}/>)
 
     const forgotPassword = screen.getByText("Forgot password?");
-    const noAccountYet = screen.getByText(" Don&apos;t have an account? Sign Up");
+    const noAccountYet = screen.getByText("Don't have an account? Sign Up");
 
     expect(forgotPassword).toBeInTheDocument()
+    expect(noAccountYet).toBeInTheDocument()
   })
 })
--- pages/login.tsx
+++ pages/login.tsx
@@ -6,6 +6,6 @@ export default function Login() {
   const { next } = router.query as any;
 
   return(
-    <SignIn forgotPassword="Forgot password?" title="ETD - Sign In" metaDescription="Sign in to ETD" noAccountYet=" Don&apos;t have an account? Sign Up" next={next} router={router}/>
+    <SignIn forgotPassword="Forgot password?" title="ETD - Sign In" metaDescription="Sign in to ETD" noAccountYet=" Don't have an account? Sign Up" next={next} router={router}/>
   )
 }
