d43ddc4ea1d52edd7ae17acfa52dc2a3c883d1f3
Vibeskanzler <tonyhawkz117@gmail.com>
2022-08-02 15:54:12 +0200

    feat(UI): change copyright text

--- src/Copyright.tsx
+++ src/Copyright.tsx
@@ -5,14 +5,7 @@ import MuiLink from '@mui/material/Link';
 export default function Copyright(props) {
   return (
     <Typography variant="body2" color="text" align="center" {...props}>
-      {'Made with 💪 by '}
-      <MuiLink color="inherit" href="https://github.com/ayham291">
-        Ayham
-      </MuiLink>{' '}
-      and{' '}
-      <MuiLink color="inherit" href="https://www.instagram.com/raphael_bgr_9900/">
-      Raphi
-      </MuiLink>{' '}
+      {'Made with 💪 by ETD-Team '}
       {new Date().getFullYear()} - {'©'}.
     </Typography>
   );
