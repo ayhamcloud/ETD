6eef07c3358e9dc9ae73b1237f7ebea588653343
ayham291 <ayham.dev@ayhamcloud.de>
2022-08-03 23:54:49 +0200

    fix(UI): check if user logged in and change buttons

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,17 @@ import { Box, Typography, Link, Button } from "@mui/material";
 import NextLink from "next/link";
 import Head from "next/head";
 import Copyright from "src/Copyright";
+import { useEffect, useContext, useState } from "react";
+import { Store } from "utils/Store";
 
 export default function Home() {
+  const { state } = useContext(Store);
+  const { userInfo } = state;
+  const [loggedIn, setLoggedIn] = useState(false);
+  useEffect(() => {
+    userInfo && setLoggedIn(true);
+  }, [userInfo]);
+
   return (
     <>
       <Head>
@@ -36,28 +45,42 @@ export default function Home() {
         <Typography component="h1" variant="subtitle1">
           The Best Place to Save Your Workouts and Visualize Your Progress!
         </Typography>
-        <NextLink href="/signup" passHref>
-          <Button
-            type="submit"
-            variant="contained"
-            sx={{ mt: 5, mb: 3 }}
-            size="large"
-          >
-            Sign Up
-          </Button>
-        </NextLink>
-        <NextLink href="/login" passHref>
-          <Button
-            type="submit"
-            // fullWidth
-            variant="outlined"
-            sx={{ mt: 3, mb: 2 }}
-            size="large"
-          // disabled={isSubmitting}
-          >
-            Log In
-          </Button>
-        </NextLink>
+        {!loggedIn ? (
+          <>
+            <NextLink href="/signup" passHref>
+              <Button
+                type="submit"
+                variant="contained"
+                sx={{ mt: 5, mb: 3 }}
+                size="large"
+              >
+                Sign Up
+              </Button>
+            </NextLink>
+            <NextLink href="/login" passHref>
+              <Button
+                type="submit"
+                variant="outlined"
+                sx={{ mt: 3, mb: 2 }}
+                size="large"
+              >
+                Log In
+              </Button>
+            </NextLink>
+          </>
+        ) : (
+          <NextLink href="/sessions" passHref>
+            <Button
+              type="submit"
+              // fullWidth
+              variant="outlined"
+              sx={{ mt: 3, mb: 2 }}
+              size="large"
+            >
+              My Sessions
+            </Button>
+          </NextLink>
+        )}
       </Box>
       <Copyright />
     </>
