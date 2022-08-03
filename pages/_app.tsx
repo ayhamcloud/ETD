0d71b86a9f7f9216a4d29f1f9689489ba7646344
maxibaxi16 <maxi.binninger@web.de>
2022-08-03 12:53:50 +0200

    feat(UI): add check email add Snackbarprovider in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import '@/styles/global.css'
 import type { AppProps } from 'next/app'
+import { SnackbarProvider } from "notistack";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  return (
+    <SnackbarProvider maxSnack={3}>
+      <Component {...pageProps} />
+    </SnackbarProvider>
+
+  )
 }
